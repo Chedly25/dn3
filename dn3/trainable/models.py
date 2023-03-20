3d49d536f3e0b5f0bd7ae336b216aafe403e87fb
Chedly25 <101675971+Chedly25@users.noreply.github.com>
2023-03-20 17:03:38 +0100

    uu

--- dn3/trainable/models.py
+++ dn3/trainable/models.py
@@ -111,6 +111,7 @@ class Classifier(DN3BaseModel):
         self.load_state_dict(self._init_state)
 
     def forward(self, *x):
+        print(*x)
         features = self.features_forward(*x)
         if self.return_features:
             return self.classifier_forward(features), features
