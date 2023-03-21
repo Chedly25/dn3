681106c6d0fb583b751d9b16eb7121c938b32790
Chedly25 <101675971+Chedly25@users.noreply.github.com>
2023-03-21 12:26:41 +0100

    edz

--- dn3/trainable/processes.py
+++ dn3/trainable/processes.py
@@ -222,7 +222,7 @@ class BaseProcess(object):
         Loss :
              Single loss quantity to be minimized.
         """
-        print(inputs)
+        print(inputs.shape)
         if isinstance(outputs, (tuple, list)):
             device = outputs[0].device
         else:
