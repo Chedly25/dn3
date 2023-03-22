08b02b9c71e2570de11ebd8d0a78c936ec33f6f5
Chedly25 <101675971+Chedly25@users.noreply.github.com>
2023-03-22 17:45:22 +0100

    asaeeeee

--- dn3/configuratron/config.py
+++ dn3/configuratron/config.py
@@ -584,6 +584,7 @@ class DatasetConfig:
                         if len(ann) > 1:
                             print("More than one annotation found for {}. Falling back to {}".format(patt, ann[0]))
                         raw.set_annotations(read_annotations(ann[0]))
+                print(self.events)
                 epochs = make_epochs_from_raw(raw, self.tmin, tlen, event_ids=self.events, baseline=self.baseline,
                                               decim=self.decimate, filter_bp=self.bandpass, drop_bad=self.drop_bad,
                                               use_annotations=use_annotations, chunk_duration=self.chunk_duration)
